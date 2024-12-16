7d32d9cf4fc467a38247c2d2ffb467db7e0ae99a
yilipr <elijfigueroaperez@gmail.com>
2024-12-15 22:16:39 -0500

    fixed equation

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -7,18 +7,34 @@
  *
  * Return: The balance factor of the tree, or 0 if tree is NULL
  */
-
 int binary_tree_balance(const binary_tree_t *tree)
 {
-
-	int left_height, right_height;
+	size_t left_height, right_height;
 
 	if (tree == NULL)
 		return (0);
 
-	left_height = tree->left ? binary_tree_height(tree->left) + 1 : 0;
-	right_height = tree->right ? binary_tree_height(tree->right) + 1 : 0;
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
-	return (left_height - right_height);
+	return ((int)(left_height - right_height));
 }
 
+/**
+ * binary_tree_height - Measures the height of a binary tree
+ * @tree: A pointer to the root node of the tree to measure the height of
+ *
+ * Return: The height of the tree, or 0 if the tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height, right_height;
+
+	if (tree == NULL)
+		return (0);
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	return ((left_height > right_height ? left_height : right_height) + 1);
+}
